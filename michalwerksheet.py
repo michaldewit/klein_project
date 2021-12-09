bf5e45ce5cea2ac2d7e9d54d626817d6d16afb4b
michaldewit <michaldewit@gmail.com>
2021-12-09 15:20:57 +0100

    functie 1 en 2 af, verder met 3

--- michalwerksheet.py
+++ michalwerksheet.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 dtf = pd.read_csv(r"C:\Users\arie\academy\projectmetsuus\uitslagen.csv", sep=";")
 # print(dtf)
-# print(dtf.columns)
+#print(dtf.columns)
 #allecodes = dtf[['RegioCode', 'GeldigeStemmen']][5:12]
 
 #allecodes.info()
@@ -11,6 +11,10 @@ dtf = pd.read_csv(r"C:\Users\arie\academy\projectmetsuus\uitslagen.csv", sep=";"
 # allecodes = dtf
 # print(allecodes[3:4])
 # print(allecodes)
+
+# 1. Nu willen we een naam invoeren Bijvoorbeeld VVD en dan zien we in welke regiocodes hij stemmen heeft gehad Ook voor Volt.
+#  Dus we komen zo te weten in hoeveel regiocodes een partij stemmen heeft gekregen
+
 def partij_checker(): #checkt of partij bestaat, en indien bestaat geeft de partij als return
     # Nu willen we een naam invoeren Bijvoorbeeld VVD
     print("geef naam van partij op, om te stoppen zeg quit ")
@@ -21,8 +25,7 @@ def partij_checker(): #checkt of partij bestaat, en indien bestaat geeft de part
         input_info = input() #verwacht een naam van een partij
         if input_info in mogelijke_naam_partij:
             print("naam is correct, gaat door")
-            partij_return = input_info
-            return partij_return 
+            return input_info 
             break
         elif input_info == "quit":
             print("programma is gestopt")
@@ -32,16 +35,72 @@ def partij_checker(): #checkt of partij bestaat, en indien bestaat geeft de part
             print("\n geef naam van partij op, om te stoppen zeg quit ")
         #print("correct")#checken of naam inderdaad van een partij is
 
-print(f'dit is de partij die je hebt gekozen {partij_checker()}')
+#print(f'dit is de partij die je hebt gekozen {partij_checker()}')
+
+def regio_codes(partij): #dataframe maken waarbij regiocode en partij uit input staat.
+    new_dtf_regiocodes = dtf.loc[:,["RegioNaam","RegioCode",partij]]
+    print(new_dtf_regiocodes.dropna(0))
+    print(f'aantal gemeentes met stemmen voor deze partij is {new_dtf_regiocodes[new_dtf_regiocodes.columns[2]].count()}')
 
 
-# 1. Nu willen we een naam invoeren Bijvoorbeeld VVD en dan zien we in welke regiocodes hij stemmen heeft gehad Ook voor Volt.
-#  Dus we komen zo te weten in hoeveel regiocodes een partij stemmen heeft gekregen
+
 # 2.  print top 3 partijen met aantal stemmen per gemeente. begin met 1 gemeente, veralgemeniseer
-# 3. alle gemeente met (letter) waarbij de partij die het grootste als eerst genoemd wordt 
+#beginnen met een functie die op row basis itereert
+
+def gemeente_top_3(gemeente):
+    gemeente_top_3 = dtf[dtf['RegioNaam'] == gemeente]
+    gemeente_dtf = gemeente_top_3.iloc[:,10:46]
+    gemeente_series = gemeente_dtf.squeeze()
+    n_string = '\n'
+    print(f'dit zijn de grootste 3 partijen in {gemeente} van groot naar klein:{n_string}R{gemeente_series.sort_values(ascending=False).head(3)}')
+
+
+def gemeente_checker(): #checkt of gemeente bestaat
+    print("geef naam van gemeente op, om te stoppen zeg quit ")
+    
+    gemeente_namen = dtf.iloc[:, 0]
+    mogelijke_naam_gemeente = list(gemeente_namen)
+    while True: #while functie om voor 
+         input_info = input() #verwacht een naam van een partij
+         if input_info in mogelijke_naam_gemeente:
+             print("naam is correct, gaat door naar volgende functie")
+             return input_info 
+             break
+         elif input_info == "quit":
+             print("programma is gestopt")
+             break
+         else:
+             print(f"naam van gemeente was incorrect, kies een gemeente uit lijst {mogelijke_naam_gemeente}")
+             print("\n geef naam van gemeente op, om te stoppen zeg quit ")
+#print(gemeente_top_3(gemeente_checker()))
+
+
+#print_stemmen()
+
+def start():
+    while True:
+        print("typ 1 om te zien in welke gemeente een ingevoerde partij stemmen heeft gehad \ntyp 2 om top 3 partijen met aantal stemmen per ingevoerde gemeente te zien")
+        invoer=input()
+        if invoer == "1":
+            print(f'{regio_codes(partij_checker())}')
+        elif invoer == "2":
+            print(gemeente_top_3(gemeente_checker()))
+        elif invoer == 'quit':
+            print('programma gestopt')
+            break
+        else:
+            print("je hebt een fout gemaakt!")
+
+start()
+
+# 3. alle gemeente met (letter) waarbij de partij die het grootste als eerst genoemd wordt
+def gemeente_letter(): #checkt of gemeente bestaat
+    print("geef letter waarmee de gemeente naam begint ")
+    
+    gemeente_namen = dtf.iloc[:, 0]
+    mogelijke_naam_gemeente = list(gemeente_namen)
+    gemeente for gemeente in mogelijke_naam_gemeente
 # 4. een overzicht per OudeRegioNaam met op STEDEN GESORTEERD met alleen de naam van de GROOTSTE PARTIJ per regionaam GEGROEPEERD op winnaar
 # 5. hoeveel procent een partij van de geldige stemmen in een regio gehaald heeft
 # 6. hoeveel procent de coalitie in handen heeft per stad
 # 7. het gemiddelde aantal kiesgerechtigde in steden die een d in de naam hebben
-
-#print_stemmen()
